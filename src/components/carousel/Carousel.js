5cd9e355cebb0edd0935c074d31908985c89d631
Sergo Gabunia <sergogabunia@gmail.com>
2021-04-07 20:03:53 +0400

    added touch events

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -5,6 +5,7 @@ import "./carousel.css";
 const Carousel = ({ children }) => {
   const [index, setIndex] = React.useState(2);
   const [length, setLength] = React.useState(children.length);
+  const [touchPosition, setTouchPosition] = React.useState(null);
 
   React.useEffect(() => {
     setLength(children.length);
@@ -18,6 +19,28 @@ const Carousel = ({ children }) => {
     index < length - 1 && setIndex((prevState) => prevState + 1);
   };
 
+  const handleTouchStart = (e) => {
+    const touchCoordinates = e.touches[0].clientX;
+    setTouchPosition(touchCoordinates);
+  };
+
+  const handleTouchMove = (e) => {
+    if (touchPosition === null) return;
+
+    const currentTouch = e.touches[0].clientX;
+    const difference = touchPosition - currentTouch;
+
+    if (difference > 5) {
+      handleNext();
+    }
+
+    if (difference < -5) {
+      handlePrevious();
+    }
+
+    setTouchPosition(null);
+  };
+
   return (
     <>
       <div className="carousel">
@@ -25,7 +48,11 @@ const Carousel = ({ children }) => {
           <Button handleButtonClick={handlePrevious} direction="previous" />
         ) : null}
 
-        <div className="carousel-items-container">
+        <div
+          className="carousel-items-container"
+          onTouchStart={handleTouchStart}
+          onTouchMove={handleTouchMove}
+        >
           <div
             className="carousel-items"
             style={{ transform: `translateX(-${index * 100}%)` }}
